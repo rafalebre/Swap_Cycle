e76a2cbc1277ca08251e00930f9b4899d6dee822
Rafael Lebre <rafaellebre100@gmail.com>
2024-03-25 17:46:09 +0100

    registration and login finished

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from . import db
 from .models import User
 
@@ -31,7 +31,6 @@ def register():
     return jsonify({"message": "User created successfully"}), 201
 
 @auth_blueprint.route('/login', methods=['POST'])
-
 def login():
     data = request.get_json()
     email = data.get('email')
@@ -46,3 +45,34 @@ def login():
         return jsonify(access_token=access_token), 200
 
     return jsonify({"error": "Invalid credentials"}), 401
+
+@auth_blueprint.route('/update', methods=['PUT'])
+@jwt_required()
+def update_user():
+    current_user_email = get_jwt_identity()
+    user = User.query.filter_by(email=current_user_email).first()
+
+    if user is None:
+        return jsonify({"error": "User not found"}), 404
+
+    data = request.get_json()
+    name = data.get('name')
+    surname = data.get('surname')
+    birth_date = data.get('birth_date')
+    profile_picture = data.get('profile_picture')
+    # Considerar a inclusão do endereço
+
+    # Atualiza os campos se foram fornecidos na requisição
+    if name:
+        user.name = name
+    if surname:
+        user.surname = surname
+    if birth_date:
+        user.birth_date = birth_date
+    if profile_picture:
+        user.profile_picture = profile_picture
+    # Atualizar o endereço aqui se aplicável
+
+    db.session.commit()
+
+    return jsonify({"message": "User information updated successfully"}), 200
--- app/models.py
+++ app/models.py
@@ -5,12 +5,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(100), unique=True, nullable=False)
-
     password_hash = db.Column(db.String(128))
     name = db.Column(db.String(100), nullable=True)
     surname = db.Column(db.String(100), nullable=True)
     birth_date = db.Column(db.DateTime, nullable=True)
     profile_picture = db.Column(db.String(255), nullable=True)
+    address = db.Column(db.String(255), nullable=True)
     registered_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
