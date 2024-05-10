c981c9f88936cbfc1cb007ddc7fc2c30bb32ef87
Rafael Lebre <rafaellebre100@gmail.com>
2024-05-10 19:16:22 +0200

    db path correction

--- app/config.py
+++ app/config.py
@@ -1,8 +1,10 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///swap_cycle.db'
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, '..', 'instance', 'swap_cycle.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 # Configurações para JWT
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
