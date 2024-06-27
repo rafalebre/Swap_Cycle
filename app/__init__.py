7d2a876b86c3f35c9368ec7c2e57f5e4860d3b15
Rafael Lebre <rafaellebre100@gmail.com>
2024-06-27 18:25:57 +0200

    removing url for from init

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, url_for
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_admin import Admin
