d51759eb7443c28dfc35f742ecddab982ce958d8
Rafael Lebre <rafaellebre100@gmail.com>
2024-07-11 17:53:51 +0200

    Flask password

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ class Config:
     base_dir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, '..', 'instance', 'swap_cycle.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'cjhbchiweDWEuedw3')
 
 
 # Configurações para JWT
